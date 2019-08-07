319652542b9378eeb83fb06f47d2a284ff29a6f6
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-08-07 11:02:58 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver};
+
+  newDriver[key] = value;
+
+  return newDriver;
+}
+
+const newDriver = updateDriverWithKeyAndValue(driver, 'Anne Marie', '25')
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const deletedDriver = Object.assign({}, driver);
+
+  delete deletedDriver[key]
+
+  return  deletedDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+
+  return driver
+}
